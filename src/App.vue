49bd9c82fdc51f2531bf8a1010b1018b0a81bca5
PaoloTarcisio <paoloc50@gmail.com>
2024-01-23 16:49:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+    import AppHeader from './components/AppHeader.vue'
+    import AppMain from './components/AppMain.vue'
+    import AppFooter from './components/AppFooter.vue'
+
+</script>
+
+<template>
+
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+
+</template>
+
+<style>
+
+</style>
